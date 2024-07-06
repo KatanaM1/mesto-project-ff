137f717a200f3739f32f425785cf479ff62d0add
Mikhail Polyakov <avtomik-s@yandex.ru>
2024-07-06 21:29:58 +0300

    rev2.0

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,67 +3,66 @@ import { initialCards } from './cards.js';
 import { visibilityCards, deletedCard, placesList, likeBtns} from './Card.js';
 import { openModal, closeModal } from './modal.js';
 
-const modalElem = document.querySelector('.profile__edit-button');
+const profileEditTarget = document.querySelector('.profile__edit-button');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
-const popupClose = document.querySelectorAll('.popup__close');
+const popupCloseButtons = document.querySelectorAll('.popup__close');
 const addButton = document.querySelector('.profile__add-button');
 const popupNewCard = document.querySelector('.popup_type_new-card');
-const formElement = document.forms.edit_profile;
+const formEditProfile = document.forms.edit_profile;
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
-const profileTittle = document.querySelector('.profile__title');
+const profileTitle = document.querySelector('.profile__title');
 const profileDscrp = document.querySelector('.profile__description');
 const cardElement = document.forms.new_place;
-const cardTittle = cardElement.elements.place_name;
-const cardImgform = cardElement.elements.link;
-const popupButton = document.querySelectorAll('.popup__button');
+const cardTitle = cardElement.elements.place_name;
+const cardImgLink = cardElement.elements.link;
 const popupTypeImage = document.querySelector('.popup_type_image');
 const popuImageLink = popupTypeImage.querySelector('.popup__image');
 const popupImageName = popupTypeImage.querySelector('.popup__caption');
 
+function fillForm() {
+    formEditProfile.name.value = profileTitle.textContent;
+    formEditProfile.description.value = profileDscrp.textContent;
+};
 
 initialCards.forEach(function(item) {
     placesList.append(visibilityCards(item.name, item.link, deletedCard, likeBtns, openImg));
 });
 
-modalElem.addEventListener("click", function () { 
+profileEditTarget.addEventListener("click", function () { 
     openModal(popupTypeEdit); 
+    fillForm();
 });
 
 addButton.addEventListener("click", function () { 
     openModal(popupNewCard); 
 });
 
-popupClose.forEach((button) => {
+popupCloseButtons.forEach((button) => {
     const popup = button.closest('.popup');
     button.addEventListener("click", () =>
     closeModal(popup));
 });
 
-popupButton.forEach((button) => {
-    const popup = button.closest('.popup');
-    button.addEventListener("click", () =>
-    closeModal(popup));
-});
-
-function handleFormSubmit(evt) {
+function EditProfileFormSubmit(evt) {
     evt.preventDefault(); 
-    profileTittle.textContent = nameInput.value;
+    profileTitle.textContent = nameInput.value;
     profileDscrp.textContent = jobInput.value;
-    formElement.reset();
+    closeModal(popupTypeEdit);
 };
 
-formElement.addEventListener('submit', handleFormSubmit); 
+formEditProfile.addEventListener('submit', EditProfileFormSubmit); 
 
 function newCardFormSubmit(evt) {
     evt.preventDefault(); 
     const newElement = {
-        name: cardTittle.value,
-        link: cardImgform.value
+        name: cardTitle.value,
+        link: cardImgLink.value
     }; 
-    const Element = visibilityCards(newElement.name, newElement.link, deletedCard, likeBtns, openImg);
-    placesList.prepend(Element);
+    const element = visibilityCards(newElement.name, newElement.link, deletedCard, likeBtns, openImg);
+    placesList.prepend(element);
     cardElement.reset();
+    closeModal(popupNewCard);
 };
   
 cardElement.addEventListener('submit', newCardFormSubmit); 
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,22 +2,25 @@ export function openModal(popupElement) {
     popupElement.classList.add('popup_is-animated');
     setTimeout(() => { popupElement.classList.add("popup_is-opened");}, 0);
     popupElement.addEventListener('click', closePopupTarget);
-    document.addEventListener('keydown', closePopupTarget);
+    document.addEventListener('keydown', closePopupEsc);
 };
 
 export function closeModal(popupElement) {
     popupElement.classList.remove('popup_is-opened');
     setTimeout(() => { popupElement.classList.remove("popup_is-animated");}, 600);
     popupElement.removeEventListener('click', closePopupTarget);
-    document.removeEventListener('keydown', closePopupTarget);
+    document.removeEventListener('keydown', closePopupEsc);
 };
 
 function closePopupTarget(evt) {
+    if (evt.target === evt.currentTarget) {
+        closeModal(evt.target);
+    }
+};
+
+function closePopupEsc(evt) {
     if (evt.key === 'Escape') {
         closeModal(document.querySelector('.popup_is-opened'));
     }
-    if (evt.target === evt.currentTarget) {
-        closeModal(document.querySelector('.popup_is-opened'));
-    }
-}
+};
 
